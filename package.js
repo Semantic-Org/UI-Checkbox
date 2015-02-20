8d66e59e1308f3848d92dcb6344182be49533cf9
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:15:31 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,15 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-checkbox',
-  summary : 'Semantic UI - Checkbox (official): Single component release of checkbox',
-  version : '1.9.2',
+  summary : 'Semantic UI - Checkbox: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Checkbox.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'checkbox.css',
+    'checkbox.js'
+  ], 'client');
 });
