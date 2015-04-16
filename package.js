27a9a219ed122868bc0b35e0768dca06d5a1135d
Pusher Robot <jack@semantic-ui.com>
2015-04-16 12:37:29 -0400

    Updated component to version 1.12.0

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-checkbox',
   summary : 'Semantic UI - Checkbox: Single component release',
-  version : '1.11.7',
+  version : '1.12.0',
   git     : 'git://github.com/Semantic-Org/UI-Checkbox.git',
 });
 
