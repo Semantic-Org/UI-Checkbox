12e9117c6f125d06d6a301e367ea4f842388a395
Jack Lukic <jack@semantic-ui.com>
2017-03-29 06:40:41 -0400

    Updated component to version 2.2.10

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-checkbox',
   summary : 'Semantic UI - Checkbox: Single component release',
-  version : '2.2.9',
+  version : '2.2.10',
   git     : 'git://github.com/Semantic-Org/UI-Checkbox.git',
 });
 
